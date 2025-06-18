1790e3ecb6a4e929f0b4c565ad3ed2294205deef
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-17 23:11:03 -0600

    Update project for day 3 as completed
    - The project for day 3 has been completed. Now, each time the user
    moves the input ranges or selects a different color the CSS for the
    corresponding variable also changes. So, when an input value is changed,
    the spacing, blur or color of the page reacts to it.

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -1,1 +1,19 @@
 "use strict";
+// Get all input elements.
+const allInputs = document.querySelectorAll(".controls input");
+
+function handleUpdate() {
+  // The suffix is the type of unit to use for the value.
+  const suffix = this.dataset.sizing ?? "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`, // Name of the css variable.
+    this.value + suffix // Current value for the input.
+  );
+}
+
+allInputs.forEach((input) => {
+  // Shows the result only when we let go of the input range.
+  input.addEventListener("change", handleUpdate);
+  // Shows the result while moving the input range.
+  input.addEventListener("mousemove", handleUpdate);
+});

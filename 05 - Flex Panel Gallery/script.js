0bcf7cbb629c7b907b29f1089dcdfc072f0f4b6c
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-20 21:05:08 -0600

    Update project for day 5 as completed
    - The project for this day is now completed. The user can click on any
    of the panels, causing the panel to become bigger and some text to
    appear.

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+// Grab all the panels.
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+  // Expands the width of the panel pressed.
+  this.classList.toggle("open");
+}
+
+function toggleActive(e) {
+  // Show the first and last paragraph of the panel.
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);

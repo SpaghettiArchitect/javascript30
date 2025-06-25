eaacc645d8aa811bfe8a789ce81e0242045c47b4
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-24 23:45:15 -0600

    Update 9th challenge with final code
    - This challenge consisted in learning how to use some web tools in the
    browser and other methods for logging information with console.

--- 09 - Dev Tools Domination/script.js
+++ 09 - Dev Tools Domination/script.js
@@ -2,7 +2,7 @@
 
 const dogs = [
   { name: "Snickers", age: 2 },
-  { name: "hugo", age: 8 },
+  { name: "Hugo", age: 8 },
 ];
 
 function makeGreen() {
@@ -12,25 +12,57 @@ function makeGreen() {
 }
 
 // Regular
+console.log("Hello!");
 
 // Interpolated
+console.log("This is a %s string", "awesome");
 
 // Styled
+console.log("%c Some styled text", "font-size: 25px; color: red;");
 
 // warning!
+console.warn("OH NOOO!");
 
 // Error :|
+console.error("Something bad happened!");
 
 // Info
+console.info("Did you know? Your eye can see a million different colors");
 
 // Testing
+const p = document.querySelector("p");
+console.assert(p.classList.contains("bad"), "That's wrong!");
 
-// clearing
+// Clearing
+// console.clear();
 
 // Viewing DOM Elements
+console.log(p);
+console.dir(p); // Show the properties of the object.
 
 // Grouping together
+dogs.forEach((dog) => {
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} dog years old`);
+  console.groupEnd(`${dog.name}`);
+});
 
-// counting
+// Counting
+console.count("Joe");
+console.count("Joe");
+console.count("Joe");
+console.count("Joe");
 
-// timing
+// Timing: How much time does a piece of code take?
+console.time("Fetching data");
+fetch("https://api.github.com/users/SpaghettiArchitect")
+  .then((rawData) => rawData.json())
+  .then((data) => {
+    console.timeEnd("Fetching data");
+    console.log(data);
+  });
+
+// Display a nice table
+console.table(dogs);

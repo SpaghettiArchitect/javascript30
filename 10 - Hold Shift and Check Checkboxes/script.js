50cf355a3a2eea9650fab0c2251458f307540e4c
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-25 23:24:15 -0600

    Update challenge 10 with complete code
    - This challenge was about how to handle multiple events so we could
    check more than one checkbox when the shift key is being pressed.

--- 10 - Hold Shift and Check Checkboxes/script.js
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+const checkboxes = document.querySelectorAll(".inbox input[type='checkbox']");
+
+// Keep track of the last ticked checkbox.
+let lastChecked;
+
+function handleCheck(e) {
+  // Check if the shift key is being pressed AND that the checkbox
+  // is being ticked.
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    // Loop over every checkbox.
+    checkboxes.forEach((checkbox) => {
+      // Check if the current checkbox is in between the two
+      // ticked checkboxes.
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      // If the checkbox is in between the two ticked checkboxes,
+      // then also tick the current checkbox.
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+// Check if any checkbox is being ticked.
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", handleCheck)
+);

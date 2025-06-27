329db2ae06ddc36ca754c33b78fe791b80ae480d
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-26 20:49:57 -0600

    Add play/pause functionality
    - The video can now be played/paused by clicking on it or by clicking
    the play/pause button.
    
    - The button's symbol also changes each time the video is being played
    or paused.

--- 11 - Custom Video Player/script.js
+++ 11 - Custom Video Player/script.js
@@ -1,1 +1,31 @@
 "use strict";
+
+// Elements of the video.
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+
+const playButton = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const sliders = player.querySelectorAll(".player__slider");
+
+// Funtions to control the video.
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updatePlayButton() {
+  const playIcon = this.paused ? "►" : "❚ ❚";
+  playButton.textContent = playIcon;
+}
+
+// Event listeners for the elements.
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updatePlayButton);
+video.addEventListener("pause", updatePlayButton);
+playButton.addEventListener("click", togglePlay);

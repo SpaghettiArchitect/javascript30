486b8f5f23204e3e89e4c147210b5e3566d3edcc
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-28 00:16:56 -0600

    Add fullscreen functionality
    - Added a new fullscreen button.
    
    - The user can enter into fullscreen by either cliking on the button or
    by pressing the F key.
    
    - The video can be paused by pressing the space key.
    
    - The icons for play/pass and skip forward/backward have been changed.

--- 11 - Custom Video Player/script.js
+++ 11 - Custom Video Player/script.js
@@ -7,8 +7,11 @@ const progress = player.querySelector(".progress");
 const progressBar = player.querySelector(".progress__filled");
 
 const playButton = player.querySelector(".toggle");
+const playButtonIcon = playButton.querySelector("i");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const sliders = player.querySelectorAll(".player__slider");
+const fullscreenButton = player.querySelector(".fullscreen");
+const fullscreenButtonIcon = fullscreenButton.querySelector("i");
 
 // Funtions to control the video.
 
@@ -23,8 +26,11 @@ function togglePlay() {
 
 // Changes the symbol inside the play button.
 function updatePlayButton() {
-  const playIcon = this.paused ? "►" : "❚ ❚";
-  playButton.textContent = playIcon;
+  const currentIcon = playButtonIcon.classList[1];
+  const newIcon = `bi-${this.paused ? "play" : "pause"}-fill`;
+
+  playButtonIcon.classList.remove(currentIcon);
+  playButtonIcon.classList.add(newIcon);
 }
 
 // Skips forward of backward in time by the value in data-skip.
@@ -49,11 +55,40 @@ function handleVideoSlider(e) {
   video.currentTime = sliderTime;
 }
 
+// Display the player element in fullscreen when called.
+function displayFullScreen() {
+  if (document.fullscreenEnabled) {
+    changeFullscreenIcon();
+
+    // If the element is already in fullscreen mode, exit the mode
+    // and return immediately.
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+      return;
+    }
+
+    // Put the player in fullscreen mode.
+    player.requestFullscreen();
+  }
+}
+
+// Change the fullscreen icon when called.
+function changeFullscreenIcon() {
+  const currentIcon = fullscreenButtonIcon.classList[1];
+  const newIcon = `bi-${
+    document.fullscreenElement ? "fullscreen" : "fullscreen-exit"
+  }`;
+
+  fullscreenButtonIcon.classList.remove(currentIcon);
+  fullscreenButtonIcon.classList.add(newIcon);
+}
+
 // Event listeners for the elements.
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updatePlayButton);
 video.addEventListener("pause", updatePlayButton);
 video.addEventListener("timeupdate", handleProgressBar);
+document.addEventListener("keydown", (e) => e.code === "Space" && togglePlay());
 
 playButton.addEventListener("click", togglePlay);
 skipButtons.forEach((btn) => btn.addEventListener("click", skipVideo));
@@ -69,3 +104,9 @@ progress.addEventListener(
 );
 progress.addEventListener("mousedown", (e) => (mouseDown = true));
 progress.addEventListener("mouseup", (e) => (mouseDown = false));
+
+document.addEventListener(
+  "keydown",
+  (e) => e.code === "KeyF" && displayFullScreen()
+);
+fullscreenButton.addEventListener("click", displayFullScreen);

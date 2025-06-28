cb8e1589d3d93f79f23374b20853f72d6ce52ce7
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-28 01:08:05 -0600

    Update with complete code for challenge 12
    - In this challenge we implemented a program that checks if the user
    has pressed a key pattern in order (similar to the Konami Code). When
    the secret code is correctly entered, rainbows or unicorns appear on the
    page.

--- 12 - Key Sequence Detection/main.js
+++ 12 - Key Sequence Detection/main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const keysPressed = [];
+const secretCode = "rainbow";
+
+window.addEventListener("keyup", (e) => {
+  keysPressed.push(e.key.toLowerCase());
+
+  // Prevents the array from being filled with all keypresses
+  // (we don't want a keylogger). It only stores the last keypresses
+  // needed to compare the array with the secretCode.
+  keysPressed.splice(
+    -secretCode.length - 1,
+    keysPressed.length - secretCode.length
+  );
+
+  // Show random unicorn/rainbow images on the page, only if the last keys
+  // pressed are equal to the secret code.
+  if (keysPressed.join("").includes(secretCode)) {
+    cornify_add();
+  }
+});

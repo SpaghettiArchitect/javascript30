a274f8cf16f6851bb468bbf408f76a47df8a6a36
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-06-30 23:23:00 -0600

    Update with complete code for challenge 16
    - This challenge consisted in creating a text shadow that moves around
    in response to the mouse movement.

--- 16 - Mouse Move Shadow/main.js
+++ 16 - Mouse Move Shadow/main.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
+const walk = 10; // Pixel limit for the shadow.
+
+function moveShadow(e) {
+  const { offsetWidth: width, offsetHeight: height } = hero;
+  const { clientX: x, clientY: y } = e;
+
+  const xWalk = (x / width) * walk - walk / 2;
+  const yWalk = (y / height) * walk - walk / 2;
+
+  text.style.textShadow = `
+  ${xWalk}px ${yWalk}px 0 rgba(255, 0, 255, 0.7),
+  ${xWalk * -1}px ${yWalk * -1}px 0 rgba(0, 255, 255, 0.7)
+  `;
+}
+
+hero.addEventListener("mousemove", moveShadow);

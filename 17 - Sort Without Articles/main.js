59ff7c6846cfdb40f6cdf723a99aead064c48a5b
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-07-01 22:05:07 -0600

    Update challenge 17 with complete code
    - In this challenge we learnt how to sort an array of strings
    alphabetically, ignoring the articles 'a', 'an', and 'the' with the use
    of regular expressions.

--- 17 - Sort Without Articles/main.js
+++ 17 - Sort Without Articles/main.js
@@ -15,3 +15,25 @@ const bands = [
   "Anywhere But Here",
   "An Old Dog",
 ];
+
+// Remove the articles 'A', 'An' or 'The' from a string.
+function stripArticles(bandName) {
+  const regex = /^(?:a|the|an)\b/i;
+  return bandName.replace(regex, "").trim();
+}
+
+// Sort the list alphabetically, from A to Z.
+const sortedBands = bands.toSorted((a, b) => {
+  a = stripArticles(a);
+  b = stripArticles(b);
+
+  return a > b ? 1 : -1;
+});
+
+const list = document.getElementById("bands");
+
+// Add the band names as items of the HTML list.
+sortedBands.forEach((bandName) => {
+  const newItem = `<li>${bandName}</li>`;
+  list.insertAdjacentHTML("beforeend", newItem);
+});

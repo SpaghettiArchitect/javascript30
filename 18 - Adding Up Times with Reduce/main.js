581ca23d7acb1ce999d759e09708da1cd306dfc1
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-07-02 21:21:07 -0600

    Update challenge 18 with complete code
    - This challenge consisted in using map and reduce to convert an array
    of strings (representing the length of a video), into the total number
    of seconds. Then, we used divisions and modular arithmetic to obtain the
    total number of hours, minutes and seconds.

--- 18 - Adding Up Times with Reduce/main.js
+++ 18 - Adding Up Times with Reduce/main.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const timeNodes = Array.from(document.querySelectorAll("[data-time]"));
+
+const totalSeconds = timeNodes
+  .map((node) => node.dataset.time)
+  .map((timeCode) => {
+    const [min, sec] = timeCode.split(":").map(parseFloat);
+    return min * 60 + sec;
+  })
+  .reduce((total, videoSeconds) => total + videoSeconds);
+
+let secondsLeft = totalSeconds;
+
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+
+const minutes = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log("Hours:", hours);
+console.log("Minutes:", minutes);
+console.log("Seconds:", secondsLeft);

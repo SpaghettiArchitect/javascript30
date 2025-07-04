602534b7fe44619c7225bded338f8092b4c68b89
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-07-03 20:53:45 -0600

    Add video stream functionality to the page
    - The webcam stream is now displayed inside the page, inside the video
    element.
    
    - This stream is also shown in the canvas element, but just every 16ms
    as to not use too much of the system resources.

--- 19 - Webcam Fun/main.js
+++ 19 - Webcam Fun/main.js
@@ -3,5 +3,40 @@
 const video = document.querySelector(".player");
 const canvas = document.querySelector(".photo");
 const ctx = canvas.getContext("2d");
-const strip = document.querySelector(".strip");
-const snap = document.querySelector(".snap");
+const photosStrip = document.querySelector(".strip");
+const takePhotoButton = document.querySelector(".btn__photo");
+const snapSound = document.querySelector(".snap");
+
+// Show the webcam stream into the page.
+function getVideoStream() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    // Handle the promise that is returned.
+    .then((localMediaStream) => {
+      // The video stream is linked to the video element.
+      video.srcObject = localMediaStream;
+      video.play(); // Refresh the frames continuously.
+    })
+    // This happens if the user denies access to its webcam.
+    .catch((error) => console.error("An error occurred :", error));
+}
+
+// Draw a video frame to the canvas every 16ms.
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  // Return the unique ID of the interval, so it can be stopped by
+  // passing it to clearInterval().
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+getVideoStream();
+
+// Runs the painToCanvas function when the video can now be played
+// (e.g. getVideoStream has been called).
+video.addEventListener("canplay", paintToCanvas);

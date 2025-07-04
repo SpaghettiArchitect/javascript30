8881502c55253239ab8473de70092054dee58131
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-07-03 23:16:47 -0600

    Add two new filters: red filter and split colors
    - The red filter changes the color of the pixels on the canvas so the
    color red appears predominant.
    
    - The split colors filter separates each color channel a by some amount,
    creating a glitch effect.

--- 19 - Webcam Fun/main.js
+++ 19 - Webcam Fun/main.js
@@ -32,6 +32,15 @@ function paintToCanvas() {
   // passing it to clearInterval().
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+
+    // Take the pixels out.
+    let pixels = ctx.getImageData(0, 0, width, height);
+
+    // Apply some filter to the pixels.
+    pixels = splitColors(pixels);
+
+    // Put the pixels back into the canvas.
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -58,6 +67,29 @@ function takePhoto() {
   // Add the link (and image) inside the photosStrip div.
   // We can also use insertBefore or insertAdjacentElement.
   photosStrip.prepend(link);
+}
+
+// VIDEO EFFECTS
+function applyRedFilter(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    // We don't modify the red value.
+    pixels.data[i + 1] = pixels.data[i + 1] * 0.25; // Green.
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.25; // Blue.
+  }
+  return pixels;
+}
+
+function splitColors(pixels) {
+  const redOffset = 20;
+  const greenOffset = -10;
+  const blueOffset = 10;
+
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + redOffset * 4] = pixels.data[i + 0]; // Red.
+    pixels.data[i + greenOffset * 4] = pixels.data[i + 1]; // Green.
+    pixels.data[i + blueOffset * 4] = pixels.data[i + 2]; // Blue.
+  }
+  return pixels;
 }
 
 getVideoStream();

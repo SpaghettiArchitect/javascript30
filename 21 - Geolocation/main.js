04254ccddb2d44c7125ed94c7134991b4742dc89
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-07-05 20:52:30 -0600

    Update challenge with complete code
    - This challege taught how to use the navigator geolocation to display
    the speed and position of the user.

--- 21 - Geolocation/main.js
+++ 21 - Geolocation/main.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const arrow = document.querySelector(".arrow");
+const speed = document.querySelector(".speed-value");
+
+navigator.geolocation.watchPosition(
+  (data) => {
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  (err) => {
+    console.error(err);
+    alert("You need to allow access to your location.");
+  }
+);

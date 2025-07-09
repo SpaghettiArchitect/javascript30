508bf0b3ab68cb07873b46ed460adbe2bdfbf18c
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-07-08 21:12:26 -0600

    Update challenge 24 with complete code
    - This challenge taught how to use the scrolling value of the window
    element to fix the position of the navigation menu.

--- 24 - Sticky Nav/main.js
+++ 24 - Sticky Nav/main.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const nav = document.querySelector("#main");
+const navTop = nav.offsetTop;
+
+// Fix the nav when the user scrolls past it.
+function fixNav() {
+  if (window.scrollY >= navTop) {
+    // Add extra space at the start of the document where the nav was.
+    document.body.style.paddingTop = `${nav.offsetHeight}px`;
+
+    // Modifies the style of the document so the nav becomes fixed, the
+    // logo appears and the document scales up a little.
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.style.paddingTop = "0px";
+    document.body.classList.remove("fixed-nav");
+  }
+}
+
+window.addEventListener("scroll", fixNav);

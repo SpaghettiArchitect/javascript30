a89e0d3170cdf9d327ec9056726e93a2c11a5018
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-07-09 19:57:07 -0600

    Update challenge with complete code
    - This challenge taught us about some of the options that can be used
    when we add a event listener to an element, and the phases of event
    propagation.

--- 25 - Event Capture, Propagation, Bubbling and Once/main.js
+++ 25 - Event Capture, Propagation, Bubbling and Once/main.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const divs = document.querySelectorAll("div");
+const button = document.querySelector("button");
+
+function logText(e) {
+  console.log(this.classList.value);
+  e.stopPropagation(); // Stop bubbling the event on parent elements.
+  // console.log(this);
+}
+
+divs.forEach((div) =>
+  div.addEventListener("click", logText, {
+    // Trigger the event on the capture phase or on the bubbling phase.
+    capture: false,
+    // Listen for the event once, and then remove the event listener.
+    once: true,
+  })
+);
+
+button.addEventListener(
+  "click",
+  () => {
+    console.log("This event only happens ONCE!");
+  },
+  { once: true }
+);

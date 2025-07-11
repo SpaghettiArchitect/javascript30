c04216473ceb7eeec4caf45105415c406980bf29
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-07-10 21:02:05 -0600

    Update challenge 26 with complete code
    - This challenge taught how to create a transition effect for the
    navigation list of the page, so it follows around the mouse when we
    hover over any of the elements in the nav.

--- 26 - Stripe Follow Along Nav/main.js
+++ 26 - Stripe Follow Along Nav/main.js
@@ -1,1 +1,54 @@
 "use strict";
+
+const triggers = document.querySelectorAll(".cool > li");
+const background = document.querySelector(".dropdownBackground");
+const nav = document.querySelector(".top");
+
+// Show the dropdown when the user hovers over the element.
+function handleEnter() {
+  // Show the div for the current element.
+  this.classList.add("trigger-enter");
+  // We need to use and arrow function because it inherits 'this' from
+  // the lexical context (e.g. this function declaration).
+  setTimeout(() => {
+    if (this.classList.contains("trigger-enter")) {
+      this.classList.add("trigger-enter-active");
+    }
+  }, 150);
+  background.classList.add("open");
+
+  // Get the coordinates for the current dropdown and the nav.
+  // The navCoords are necessary so the dropdown is correctly
+  // positioned even if we add more elements before the nav element.
+  const dropdown = this.querySelector(".dropdown");
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  const navCoords = nav.getBoundingClientRect();
+
+  // Calculate the current size of the div container.
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left,
+  };
+
+  // Set the size and position of the background div on the current
+  // list element.
+  background.style.width = `${coords.width}px`;
+  background.style.height = `${coords.height}px`;
+  background.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+// Hide the dropdown when the user hover out of the element.
+function handleLeave() {
+  this.classList.remove("trigger-enter", "trigger-enter-active");
+  background.classList.remove("open");
+}
+
+// Add triggers for each element of the nav list.
+triggers.forEach((trigger) =>
+  trigger.addEventListener("mouseenter", handleEnter)
+);
+triggers.forEach((trigger) =>
+  trigger.addEventListener("mouseleave", handleLeave)
+);

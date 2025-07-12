dec12e7f62376be4b9872d5907d3c137eb146cbe
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-07-11 22:59:06 -0600

    Update with complete code for challenge 27
    - This challenge taught how to create an horizontal slider that can be
    dragged by pressing and moving the mouse.

--- 27 - Click and Drag/main.js
+++ 27 - Click and Drag/main.js
@@ -1,1 +1,32 @@
 "use strict";
+
+const slider = document.querySelector(".items");
+let isMouseDown = false;
+let startX, scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isMouseDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+  isMouseDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseup", () => {
+  isMouseDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  // Stop the function from running.
+  if (!isMouseDown) return;
+  e.preventDefault();
+
+  const mouseX = e.pageX - slider.offsetLeft;
+  const walk = (mouseX - startX) * 3;
+  slider.scrollLeft = scrollLeft - walk;
+});

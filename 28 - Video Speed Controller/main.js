89ca706525f88ac8627c4922cabbaea63be04c35
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-07-12 21:24:20 -0600

    Update with complete code for challenge 28
    - This challenge taught how to create a custom video speed slider, so
    the user can change the playbackRate of a video element by moving their
    mouse on a bar.

--- 28 - Video Speed Controller/main.js
+++ 28 - Video Speed Controller/main.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const videoSpeed = document.querySelector(".speed");
+const speedBar = videoSpeed.querySelector(".speed-bar");
+const video = document.querySelector(".flex");
+
+videoSpeed.addEventListener("mousemove", function (e) {
+  // VISUAL PART: movement of the bar.
+
+  // Current mouse position on the bar and the percentage that represents.
+  const mouseY = e.pageY - this.offsetTop;
+  const percent = mouseY / this.offsetHeight;
+
+  // Values for the playback speed.
+  const minSpeed = 0.5;
+  const maxSpeed = 2.5;
+
+  // Calulate the value of the playbackRate, so it doesn't go below minSpeed
+  // or above maxSpeed.
+  const playbackRate = (percent * (maxSpeed - minSpeed) + minSpeed).toFixed(1);
+
+  // Display the speed of the video and move the height of the bar to
+  // the position of the mouse.
+  const heightString = Math.round(percent * 100) + "%";
+  speedBar.textContent = playbackRate + "\u00D7";
+  speedBar.style.height = heightString;
+
+  // FUNCTIONAL PART: video speed control.
+  video.playbackRate = Number(playbackRate);
+});

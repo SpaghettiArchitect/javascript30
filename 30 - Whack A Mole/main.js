c5bc1ba440f124d85ba39f7de783a8cadb82c677
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-07-14 21:01:15 -0600

    Update challenge 30 with complete code
    - In this challenge we made a simple game were we put in practice the
    things learned from the other challenges. This included calculating
    ranges, using event listeners, setting intervals and timeouts, getting
    elements from the page and setting their content, etcetera.

--- 30 - Whack A Mole/main.js
+++ 30 - Whack A Mole/main.js
@@ -3,3 +3,62 @@
 const holes = document.querySelectorAll(".hole");
 const scoreBoard = document.querySelector(".score");
 const moles = document.querySelectorAll(".mole");
+const startBtn = document.querySelector("button");
+
+let lastHole;
+let timeUp = false;
+let score = 0;
+
+// Get a random number for the given range.
+function randomTime(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+  const index = Math.floor(Math.random() * holes.length);
+  const hole = holes[index];
+
+  // Check the new hole for the mole is not the same as the last one.
+  if (hole === lastHole) {
+    return randomHole(holes);
+  }
+
+  lastHole = hole;
+  return hole;
+}
+
+function peepMole() {
+  const time = randomTime(250, 1000);
+  const hole = randomHole(holes);
+
+  hole.classList.add("up");
+
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peepMole();
+  }, time);
+}
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  score = 0;
+  timeUp = false;
+  startBtn.style.display = "none";
+  peepMole();
+
+  setTimeout(() => {
+    timeUp = true;
+    startBtn.style.display = "block";
+  }, 10000);
+}
+
+function bonkMole(e) {
+  // If the player tries to cheat the click, return.
+  if (!e.isTrusted) return;
+  score++;
+  // Hide the mole if is clicked.
+  this.classList.remove("up");
+  scoreBoard.textContent = score;
+}
+
+moles.forEach((mole) => mole.addEventListener("click", bonkMole));
